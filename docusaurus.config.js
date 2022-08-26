c9e17f92e360bb3c7c14688f3a483d3680133428
Jordan Eggers <jordaneggers@Jordans-MBP.lan>
2022-08-26 13:35:42 -0400

    fix footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,15 +71,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
